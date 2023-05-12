a78a22bbaa8e6e6dace1d30dcd9c38d06afd0e41
Amar Cedic <amarcedic2605@gmail.com>
2023-05-12 10:48:23 +0200

    Add Tic Tac Toe game

---
+++ tictactoe.js
@@ -0,0 +1,18 @@
+var board = Array(9).fill(null);
+var currentPlayer = 'X';
+
+document.querySelectorAll('.cell').forEach((cell, i) => {
+  cell.addEventListener('click', function() {
+    if (!board[i]) {
+      board[i] = currentPlayer;
+      cell.textContent = currentPlayer;
+      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    }
+  });
+});
+
+document.getElementById('reset-button').addEventListener('click', function() {
+  board.fill(null);
+  document.querySelectorAll('.cell').forEach(cell => cell.textContent = '');
+  currentPlayer = 'X';
+});
